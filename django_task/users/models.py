d134b3b6cea47cb00e200fdacf6688b4341d42fa
patryk_s <boydmalvo@gmail.com>
2022-09-05 09:47:21 +0200

    add CustomUser and migrations

--- django_task/users/models.py
+++ django_task/users/models.py
@@ -1,3 +1,9 @@
+from random import randint
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    birthday = models.DateField(default=False)
+    random_number = models.IntegerField(randint(1,100))
